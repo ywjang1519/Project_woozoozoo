31036d0ebddc8652bcfda85e4b6853d828a7502a
Yongwon Jang <ywjang1519@gmail.com>
2022-12-15 10:21:43 +0900

    플라스크에 AI 모델 로드 할 수 있게 변경

--- Flask/pywzz/views/check_views.py
+++ Flask/pywzz/views/check_views.py
@@ -1,5 +1,36 @@
 from flask import Blueprint, render_template, request, jsonify, session, redirect, url_for
 from werkzeug.utils import secure_filename
+from torchvision import transforms
+import torch
+from PIL import Image
+import io
+
+# AI부분 
+
+model = torch.load('./model/model.pth')
+
+def transform_image(image_bytes):
+    transforms_img = transforms.Compose([
+        transforms.Resize((224, 224)),
+        transforms.ToTensor(),
+        transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
+    ])
+    img = Image.open(io.BytesIO(image_bytes))
+    return transform_image(img).unsqueeze(0)
+
+def predict_image(image_bytes,model):
+    device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu") # device 객체 생성
+    class_model = ['구진/플라크','비듬/각질/상피성잔고리','태선화/과다색소침착','농포/여드름','미란/궤양','결절,종괴']
+    tensor_img = transform_image(image_bytes).to(device)
+    model.eval()
+    with torch.no_grad():
+        outputs = model(tensor_img)
+        _, preds = torch.max(outputs, 1)
+    return class_model[preds]
+
+
+# --------------------------
+# 이미지 업로드 부분
 
 ALLOWED_EXTENSIONS = set(['png', 'jpg', 'jpeg', 'gif'])
 
@@ -8,6 +39,10 @@ bp = Blueprint('check', __name__, url_prefix='/check_home')
 def allwed_file(filename):
     # .이 있나 없나 체크하는 것과 확장자 확인, 되면 1, 안되면 0
     return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
+#------------------------------------------------------
+
+#Flask 주소 부분
 
 @bp.route('/check')
 def check():
