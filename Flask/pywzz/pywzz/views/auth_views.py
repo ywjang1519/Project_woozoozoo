4d05358f2c2f67cef4b8753c0ce621d0d7abad54
skiLee <chocomi77@gmail.com>
2022-12-20 09:45:10 +0900

    1220 09:45 - 어제 login> logout 이렇게 바뀌는거 함!

--- Flask/pywzz/pywzz/views/auth_views.py
+++ Flask/pywzz/pywzz/views/auth_views.py
@@ -1,4 +1,4 @@
-from flask import Blueprint,render_template,request,url_for,flash,session
+from flask import Blueprint,render_template,request,url_for,flash,session, g
 from pywzz.forms import UserCreateForm,UserLoginForm
 from pywzz import db
 from pywzz.models import User
@@ -11,13 +11,8 @@ bp=Blueprint('auth',__name__,url_prefix='/auth')
 @bp.route('signup',methods=('GET','POST'))
 def signup():
     form=UserCreateForm()
-
-    print('----1. 유저 아이디')
     if request.method=='POST' and form.validate_on_submit():
-        print('----2. 이미 존재하는 지?')
         user=User.query.filter_by(user_name=form.username.data).first()
-
-        print('----3.')
         if not user:
             user=User(user_name=form.username.data, user_password=generate_password_hash(form.password1.data),
                       user_email=form.email.data, user_phone=form.phone.data )
@@ -36,11 +31,26 @@ def login():
         user = User.query.filter_by(user_name=form.username.data).first()
         if not user:
             error = "존재하지 않는 사용자입니다."
-        elif not check_password_hash(user.password, form.password.data):
+        elif not check_password_hash(user.user_password, form.password.data):
             error = "비밀번호가 올바르지 않습니다."
+
         if error is None:
             session.clear()
-            session['user_id'] = user.id
+            session['user_name'] = user.user_name
             return redirect(url_for('main.index'))
         flash(error)
     return render_template('auth/login_bts.html', form=form)
+
+
+@bp.before_app_request
+def load_logged_in_user():
+    user_name = session.get('user_name')
+    if user_name is None:
+        g.user = None
+    else:
+        g.user = user_name
+
+@bp.route('/logout/')
+def logout():
+    session.clear()
+    return redirect(url_for('main.home'))
