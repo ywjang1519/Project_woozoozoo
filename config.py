d5dd3841f7e6c7c6a0f09fee03f999efcb032fc8
[github <hioka1211@gmail.com>
2023-01-10 08:58:42 +0900

    no message

--- config.py
+++ config.py
@@ -2,7 +2,7 @@ import os
 
 BASE_DIR = os.path.dirname(__file__)
 
-SQLALCHEMY_DATABASE_URI='mysql+pymysql://3pos:threepos@192.168.55.8:3306/3pos'
+SQLALCHEMY_DATABASE_URI='mysql+pymysql://3pos:threepos@192.168.6.101:3306/3pos'
 #SQLALCHEMY_DATABASE_URI='mysql+pymysql://devkiki:910716@210.57.238.194:8572/kiki?useUnicode=true&characterEncoding=utf8&autoReconnect=true&zeroDateTimeBehavior=convertToNull'
 # sql 주소 'sqllite:///{}'
 SQLALCHEMY_MODIFICATIONS = False
@@ -10,7 +10,7 @@ SECRET_KEY="dev" #원래는 길게 작성하지만, test용이니 dev라고 함
 
 from sqlalchemy import create_engine
 import sqlalchemy as db
-engine = db.create_engine('mysql+pymysql://3pos:threepos@192.168.55.8:3306/3pos')
+engine = db.create_engine('mysql+pymysql://3pos:threepos@192.168.6.101:3306/3pos')
 # from sqlalchemy import create_engine
 # engine = create_engine('mysql+mysqldb://<username>:<password>@<host>:<port>/<dbname>')
 connection = engine.connect()
